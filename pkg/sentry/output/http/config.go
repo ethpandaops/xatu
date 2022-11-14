249d837ba95074765bfa789657c4c5a5b5a8a27e
Andrew Davis <1709934+Savid@users.noreply.github.com>
2022-11-14 16:31:03 +1000

    fix(sentry): http max_export_batch_size config name

--- pkg/sentry/output/http/config.go
+++ pkg/sentry/output/http/config.go
@@ -8,10 +8,10 @@ import (
 type Config struct {
 	Address            string            `yaml:"address"`
 	Headers            map[string]string `yaml:"headers"`
-	MaxQueueSize       int               `yaml:"max_queue_size" default:"2048"`
+	MaxQueueSize       int               `yaml:"max_queue_size" default:"51200"`
 	BatchTimeout       time.Duration     `yaml:"batch_timeout" default:"5s"`
 	ExportTimeout      time.Duration     `yaml:"export_timeout" default:"30s"`
-	MaxExportBatchSize int               `yaml:"batch_interval_seconds" default:"512"`
+	MaxExportBatchSize int               `yaml:"max_export_batch_size" default:"512"`
 }
 
 func (c *Config) Validate() error {

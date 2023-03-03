9f30375f76094144b05c7707a8cf39c687c7046c
Andrew Davis <1709934+Savid@users.noreply.github.com>
2023-03-03 15:22:27 +1000

    fit(mimicry): raise ignore period for initial transaction pool burst

--- pkg/mimicry/p2p/execution/execution.go
+++ pkg/mimicry/p2p/execution/execution.go
@@ -198,8 +198,8 @@ func (p *Peer) Start(ctx context.Context) (<-chan error, error) {
 			"fork_id_next": fmt.Sprintf("%d", status.ForkID.Next),
 		}).Debug("got client status")
 
-		// set the ignore before time to 15 seconds in the future
-		ignoreBefore := time.Now().Add(15 * time.Second)
+		// set the ignore before time to 1 minute in the future
+		ignoreBefore := time.Now().Add(1 * time.Minute)
 		p.ignoreBefore = &ignoreBefore
 
 		return nil

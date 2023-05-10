011f19e6fd8a1efde369eb9222f10488b2499ee6
Andrew Davis <1709934+Savid@users.noreply.github.com>
2023-05-10 15:19:54 +1000

    feat(mimicry): move to network focused

--- pkg/mimicry/coordinator/xatu/coordinator/config.go
+++ pkg/mimicry/coordinator/xatu/coordinator/config.go
@@ -8,7 +8,6 @@ type Config struct {
 	Address      string            `yaml:"address"`
 	Headers      map[string]string `yaml:"headers"`
 	TLS          bool              `yaml:"tls" default:"false"`
-	NetworkIds   []uint64          `yaml:"networkIds"`
 	ForkIDHashes []string          `yaml:"forkIdHashes"`
 	MaxPeers     uint32            `yaml:"maxPeers" default:"100"`
 }
@@ -16,10 +15,6 @@ type Config struct {
 func (c *Config) Validate() error {
 	if c.Address == "" {
 		return errors.New("address is required")
-	}
-
-	if len(c.NetworkIds) == 0 {
-		return errors.New("networkIds is required")
 	}
 
 	return nil
--- pkg/mimicry/coordinator/xatu/coordinator/coordinator.go
+++ pkg/mimicry/coordinator/xatu/coordinator/coordinator.go
@@ -89,7 +89,6 @@ func (c *Coordinator) CoordinateExecutionNodeRecords(ctx context.Context, record
 	}
 
 	req := xatu.CoordinateExecutionNodeRecordsRequest{
-		NetworkIds:   c.config.NetworkIds,
 		ForkIdHashes: forkIDHashes,
 		NodeRecords:  records,
 		Limit:        c.config.MaxPeers,

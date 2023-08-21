be8f2b2db81f4f7e92814289f3f722a079eaf6f5
Andrew Davis <1709934+Savid@users.noreply.github.com>
2023-08-21 15:31:54 +1000

    feat(server): add validator attestation data

---
+++ pkg/server/service/event-ingester/event/beacon/eth/v1/validator_attestation_data.go
@@ -0,0 +1,42 @@
+package v1
+
+import (
+	"context"
+	"errors"
+
+	"github.com/ethpandaops/xatu/pkg/proto/xatu"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	ValidatorAttestationDataType = "BEACON_API_ETH_V1_VALIDATOR_ATTESTATION_DATA"
+)
+
+type ValidatorAttestationData struct {
+	log   logrus.FieldLogger
+	event *xatu.DecoratedEvent
+}
+
+func NewValidatorAttestationData(log logrus.FieldLogger, event *xatu.DecoratedEvent) *ValidatorAttestationData {
+	return &ValidatorAttestationData{
+		log:   log.WithField("event", ValidatorAttestationDataType),
+		event: event,
+	}
+}
+
+func (b *ValidatorAttestationData) Type() string {
+	return EventsHeadType
+}
+
+func (b *ValidatorAttestationData) Validate(_ context.Context) error {
+	_, ok := b.event.GetData().(*xatu.DecoratedEvent_EthV1ValidatorAttestationData)
+	if !ok {
+		return errors.New("failed to cast event data")
+	}
+
+	return nil
+}
+
+func (b *ValidatorAttestationData) Filter(_ context.Context) bool {
+	return false
+}

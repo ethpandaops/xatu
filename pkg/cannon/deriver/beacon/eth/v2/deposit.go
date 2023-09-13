6b193d04f4cd806c5adbb19616d003ba9680a95e
Sam Calder-Mason <sam@puritydev.io>
2023-09-13 17:29:16 +1000

    fix(deposit): Fix calculation of slot in processEpoch

--- pkg/cannon/deriver/beacon/eth/v2/deposit.go
+++ pkg/cannon/deriver/beacon/eth/v2/deposit.go
@@ -157,7 +157,7 @@ func (b *DepositDeriver) processEpoch(ctx context.Context, epoch phase0.Epoch) (
 	allEvents := []*xatu.DecoratedEvent{}
 
 	for i := uint64(0); i <= uint64(sp.SlotsPerEpoch); i++ {
-		slot := phase0.Slot(i + uint64(epoch))
+		slot := phase0.Slot(i + uint64(epoch)*uint64(sp.SlotsPerEpoch))
 
 		events, err := b.processSlot(ctx, slot)
 		if err != nil {

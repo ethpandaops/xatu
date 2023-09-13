59d6ccde508fa13efb478ce278732fb809f11a32
Sam Calder-Mason <sam@puritydev.io>
2023-09-13 17:17:55 +1000

    fix(cannon): Correctly calculate slot

--- pkg/cannon/deriver/beacon/eth/v2/execution_transaction.go
+++ pkg/cannon/deriver/beacon/eth/v2/execution_transaction.go
@@ -160,7 +160,7 @@ func (b *ExecutionTransactionDeriver) processEpoch(ctx context.Context, epoch ph
 	allEvents := []*xatu.DecoratedEvent{}
 
 	for i := uint64(0); i <= uint64(sp.SlotsPerEpoch); i++ {
-		slot := phase0.Slot(i + uint64(epoch))
+		slot := phase0.Slot(i + uint64(epoch)*uint64(sp.SlotsPerEpoch))
 
 		events, err := b.processSlot(ctx, slot)
 		if err != nil {

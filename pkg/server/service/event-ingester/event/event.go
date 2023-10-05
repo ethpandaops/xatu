fc07c711921d1dffaf87b42624a05f31cd81e94c
Sam Calder-Mason <sam@puritydev.io>
2023-10-05 13:02:13 +1000

    Merge master

--- pkg/server/service/event-ingester/event/event.go
+++ pkg/server/service/event-ingester/event/event.go
@@ -49,11 +49,8 @@ const (
 	TypeBeaconEthV2BeaconExecutionTransaction   Type = v2.BeaconBlockExecutionTransactionType
 	TypeBeaconEthV2BeaconBLSToExecutionChange   Type = v2.BeaconBlockBLSToExecutionChangeType
 	TypeBeaconEthV2BeaconWithdrawal             Type = v2.BeaconBlockWithdrawalType
-<<<<<<< HEAD
 	TypeBlockprintBlockClassification           Type = blockprint.BlockClassificationType
-=======
 	TypeBeaconETHV1EventsBlobSidecar            Type = v1.EventsBlobSidecarType
->>>>>>> master
 )
 
 type Event interface {
@@ -131,13 +128,10 @@ func New(eventType Type, log logrus.FieldLogger, event *xatu.DecoratedEvent, cac
 		return v2.NewBeaconBlockBLSToExecutionChange(log, event), nil
 	case TypeBeaconEthV2BeaconWithdrawal:
 		return v2.NewBeaconBlockWithdrawal(log, event), nil
-<<<<<<< HEAD
 	case TypeBlockprintBlockClassification:
 		return blockprint.NewBlockClassification(log, event), nil
-=======
 	case TypeBeaconETHV1EventsBlobSidecar:
 		return v1.NewEventsBlobSidecar(log, event), nil
->>>>>>> master
 
 	default:
 		return nil, fmt.Errorf("event type %s is unknown", eventType)

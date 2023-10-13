7be1904a7d8e9c9625c3a786335a07aea9e1613d
0x-dx <145253945+0x-dx@users.noreply.github.com>
2023-10-13 22:57:48 +0800

    feat(output/kafka-sink): Add Kafka Sink.

---
+++ pkg/output/kafka/exporter.go
@@ -0,0 +1,108 @@
+package kafka
+
+import (
+	"context"
+	"errors"
+	"github.com/IBM/sarama"
+	"github.com/ethpandaops/xatu/pkg/observability"
+	"github.com/ethpandaops/xatu/pkg/proto/xatu"
+	"github.com/sirupsen/logrus"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+type ItemExporter struct {
+	config *Config
+	log    logrus.FieldLogger
+	client sarama.SyncProducer
+}
+
+func NewItemExporter(name string, config *Config, log logrus.FieldLogger) (ItemExporter, error) {
+
+	producer, err := NewSyncProducer(config)
+
+	if err != nil {
+		log.
+			WithError(err).
+			WithField("output_name", name).
+			WithField("output_type", SinkType).
+			Error("Error while creating the Kafka Client")
+		return ItemExporter{}, err
+	}
+
+	return ItemExporter{
+		config: config,
+		log:    log.WithField("output_name", name).WithField("output_type", SinkType),
+		client: producer,
+	}, nil
+}
+func (e ItemExporter) ExportItems(ctx context.Context, items []*xatu.DecoratedEvent) error {
+	_, span := observability.Tracer().Start(ctx, "KafkaItemExporter.ExportItems", trace.WithAttributes(attribute.Int64("num_events", int64(len(items)))))
+	defer span.End()
+
+	e.log.WithField("events", len(items)).Debug("Sending batch of events to Kafka sink")
+
+	if err := e.sendUpstream(ctx, items); err != nil {
+		e.log.
+			WithError(err).
+			WithField("num_events", len(items)).
+			Error("Failed to send events upstream")
+
+		span.SetStatus(codes.Error, err.Error())
+
+		return err
+	}
+
+	return nil
+}
+
+func (e ItemExporter) Shutdown(ctx context.Context) error {
+	return nil
+}
+
+const MAX_MSG_BYTE_SIZE = 1024 * 1024
+
+func (e *ItemExporter) sendUpstream(ctx context.Context, items []*xatu.DecoratedEvent) error {
+	msgs := make([]*sarama.ProducerMessage, 0, len(items))
+	msgByteSize := 0
+	for _, p := range items {
+		r, err := protojson.Marshal(p)
+		if err != nil {
+			return err
+		}
+		routingKey := sarama.StringEncoder(p.Event.Id)
+		eventPayload := sarama.StringEncoder(r)
+		m := &sarama.ProducerMessage{
+			Topic: e.config.Topic,
+			Key:   routingKey,
+			Value: eventPayload,
+		}
+		msgByteSize = m.ByteSize(2)
+		if msgByteSize > MAX_MSG_BYTE_SIZE {
+			e.log.WithField("event_id", routingKey).WithField("msg_size", msgByteSize).Debug("Message too large, consider increasing `max_message_bytes`")
+			continue
+		}
+		msgs = append(msgs, m)
+	}
+	err := e.client.SendMessages(msgs)
+	errorCount := 0
+	if err != nil {
+		var errs sarama.ProducerErrors
+		if errors.As(err, &errs) {
+			errorCount = len(errs)
+			for _, producerError := range errs {
+				e.log.
+					WithError(producerError.Err).
+					WithField("num_events", len(errs)).
+					Error("Failed to send events to Kafka")
+				return producerError
+			}
+		}
+		return err
+	}
+	e.log.WithField("count", len(msgs)-errorCount).Debug("Items written to Kafka")
+
+	return nil
+}
---
+++ pkg/output/kafka/kafka.go
@@ -0,0 +1,103 @@
+package kafka
+
+import (
+	"context"
+	"errors"
+	"github.com/ethpandaops/xatu/pkg/processor"
+	"github.com/ethpandaops/xatu/pkg/proto/xatu"
+	"github.com/sirupsen/logrus"
+)
+
+const SinkType = "kafka"
+
+type Kafka struct {
+	name   string
+	config *Config
+	log    logrus.FieldLogger
+	proc   *processor.BatchItemProcessor[xatu.DecoratedEvent]
+	filter xatu.EventFilter
+}
+
+func New(name string, config *Config, log logrus.FieldLogger, filterConfig *xatu.EventFilterConfig, shippingMethod processor.ShippingMethod) (*Kafka, error) {
+	if config == nil {
+		return nil, errors.New("config is required")
+	}
+
+	if err := config.Validate(); err != nil {
+		return nil, err
+	}
+
+	exporter, err := NewItemExporter(name, config, log)
+	if err != nil {
+		return nil, err
+	}
+
+	filter, err := xatu.NewEventFilter(filterConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	proc, err := processor.NewBatchItemProcessor[xatu.DecoratedEvent](
+		exporter,
+		xatu.ImplementationLower()+"_output_"+SinkType+"_"+name,
+		log,
+		processor.WithShippingMethod(shippingMethod),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Kafka{
+		name:   name,
+		config: config,
+		log:    log,
+		proc:   proc,
+		filter: filter,
+	}, nil
+}
+
+func (h *Kafka) Name() string {
+	return h.name
+}
+
+func (h *Kafka) Type() string {
+	return SinkType
+}
+
+func (h *Kafka) Start(ctx context.Context) error {
+	return nil
+}
+
+func (h *Kafka) Stop(ctx context.Context) error {
+	return h.proc.Shutdown(ctx)
+}
+
+func (h *Kafka) HandleNewDecoratedEvent(ctx context.Context, event *xatu.DecoratedEvent) error {
+	shouldBeDropped, err := h.filter.ShouldBeDropped(event)
+	if err != nil {
+		return err
+	}
+
+	if shouldBeDropped {
+		return nil
+	}
+
+	return h.proc.Write(ctx, []*xatu.DecoratedEvent{event})
+}
+
+func (h *Kafka) HandleNewDecoratedEvents(ctx context.Context, events []*xatu.DecoratedEvent) error {
+	filtered := []*xatu.DecoratedEvent{}
+
+	for _, event := range events {
+		shouldBeDropped, err := h.filter.ShouldBeDropped(event)
+		if err != nil {
+			return err
+		}
+
+		if !shouldBeDropped {
+			filtered = append(filtered, event)
+		}
+	}
+
+	return h.proc.Write(ctx, filtered)
+}

00a83ae7e6ab3f67b9a415e0e388e1a460354e56
Sam Calder-Mason <sam@puritydev.io>
2024-01-16 15:41:25 +1000

    deploy(clickhouse): Drop Beacon_p2p_attestation signature

--- deploy/migrations/clickhouse/025_rm_beacon_p2p_attestation_signature.down.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE default.beacon_p2p_attestation_local ON CLUSTER '{cluster}'
-ADD COLUMN signature String Codec(ZSTD(1)) AFTER attesting_validator_committee_index;
-
-ALTER TABLE default.beacon_p2p_attestation ON CLUSTER '{cluster}'
-ADD COLUMN signature String Codec(ZSTD(1)) AFTER attesting_validator_committee_index;

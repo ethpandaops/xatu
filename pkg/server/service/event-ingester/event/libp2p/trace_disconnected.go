6417a94e05b8e55dc0b4f90e75b6d2e023e9e064
Andrew Davis <1709934+Savid@users.noreply.github.com>
2024-04-18 15:51:25 +1000

    feat(server): add libp2p (dis)connect geo meta data

--- pkg/server/service/event-ingester/event/libp2p/trace_disconnected.go
+++ pkg/server/service/event-ingester/event/libp2p/trace_disconnected.go
@@ -3,8 +3,11 @@ package libp2p
 import (
 	"context"
 	"errors"
+	"net"
+	"strings"
 
 	"github.com/ethpandaops/xatu/pkg/proto/xatu"
+	"github.com/ethpandaops/xatu/pkg/server/geoip"
 	"github.com/sirupsen/logrus"
 )
 
@@ -13,14 +16,16 @@ var (
 )
 
 type TraceDisconnected struct {
-	log   logrus.FieldLogger
-	event *xatu.DecoratedEvent
+	log           logrus.FieldLogger
+	event         *xatu.DecoratedEvent
+	geoipProvider geoip.Provider
 }
 
-func NewTraceDisconnected(log logrus.FieldLogger, event *xatu.DecoratedEvent) *TraceDisconnected {
+func NewTraceDisconnected(log logrus.FieldLogger, event *xatu.DecoratedEvent, geoipProvider geoip.Provider) *TraceDisconnected {
 	return &TraceDisconnected{
-		log:   log.WithField("event", TraceDisconnectedType),
-		event: event,
+		log:           log.WithField("event", TraceDisconnectedType),
+		event:         event,
+		geoipProvider: geoipProvider,
 	}
 }
 
@@ -42,5 +47,53 @@ func (td *TraceDisconnected) Filter(ctx context.Context) bool {
 }
 
 func (td *TraceDisconnected) AppendServerMeta(ctx context.Context, meta *xatu.ServerMeta) *xatu.ServerMeta {
+	data, ok := td.event.Data.(*xatu.DecoratedEvent_Libp2PTraceDisconnected)
+	if !ok {
+		td.log.Error("failed to get remote maddrs")
+
+		return meta
+	}
+
+	multiaddr := data.Libp2PTraceDisconnected.GetRemoteMaddrs().GetValue()
+	if multiaddr != "" {
+		return meta
+	}
+
+	addrParts := strings.Split(multiaddr, "/")
+	if len(addrParts) < 3 {
+		return meta
+	}
+
+	ipAddress := addrParts[2]
+
+	if ipAddress != "" {
+		ip := net.ParseIP(ipAddress)
+		if ip != nil && td.geoipProvider != nil {
+			geoipLookupResult, err := td.geoipProvider.LookupIP(ctx, ip)
+			if err != nil {
+				td.log.WithField("ip", ipAddress).WithError(err).Warn("failed to lookup geoip data")
+			}
+
+			if geoipLookupResult != nil {
+				meta.AdditionalData = &xatu.ServerMeta_LIBP2P_TRACE_DISCONNECTED{
+					LIBP2P_TRACE_DISCONNECTED: &xatu.ServerMeta_AdditionalLibp2PTraceDisconnectedData{
+						Peer: &xatu.ServerMeta_Peer{
+							Geo: &xatu.ServerMeta_Geo{
+								Country:                      geoipLookupResult.CountryName,
+								CountryCode:                  geoipLookupResult.CountryCode,
+								City:                         geoipLookupResult.CityName,
+								Latitude:                     geoipLookupResult.Latitude,
+								Longitude:                    geoipLookupResult.Longitude,
+								ContinentCode:                geoipLookupResult.ContinentCode,
+								AutonomousSystemNumber:       geoipLookupResult.AutonomousSystemNumber,
+								AutonomousSystemOrganization: geoipLookupResult.AutonomousSystemOrganization,
+							},
+						},
+					},
+				}
+			}
+		}
+	}
+
 	return meta
 }

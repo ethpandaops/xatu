7825464a46601735f0c8c83a627891f091752b8d
Sam Calder-Mason <sam@puritydev.io>
2024-09-13 16:33:46 +1000

    feat(output): Add filtering on module

--- pkg/proto/xatu/filter.go
+++ pkg/proto/xatu/filter.go
@@ -9,13 +9,15 @@ import (
 type EventFilter interface {
 	// EventNames returns the list of event names to filter on.
 	EventNames() []string
-
+	// Modules returns the list of modules to filter on.
+	Modules() []string
 	// ShouldBeDropped returns true if the event should be dropped.
 	ShouldBeDropped(event *DecoratedEvent) (bool, error)
 }
 
 type EventFilterConfig struct {
 	EventNames []string `yaml:"eventNames"`
+	Modules    []string `yaml:"modules"`
 }
 
 func (f *EventFilterConfig) Validate() error {
@@ -39,8 +41,16 @@ func NewEventFilter(config *EventFilterConfig) (EventFilter, error) {
 		eventNames[eventName] = struct{}{}
 	}
 
+	modules := make(map[string]struct{}, len(config.Modules))
+
+	for _, module := range config.Modules {
+		modules[module] = struct{}{}
+	}
+
 	return &eventFilter{
+		config:     config,
 		eventNames: eventNames,
+		modules:    modules,
 	}, nil
 }
 
@@ -48,10 +58,15 @@ type eventFilter struct {
 	config *EventFilterConfig
 
 	eventNames map[string]struct{}
+	modules    map[string]struct{}
 }
 
 func (f *eventFilter) EventNames() []string {
 	return f.config.EventNames
+}
+
+func (f *eventFilter) Modules() []string {
+	return f.config.Modules
 }
 
 func (f *eventFilter) ShouldBeDropped(event *DecoratedEvent) (bool, error) {
@@ -63,14 +78,36 @@ func (f *eventFilter) ShouldBeDropped(event *DecoratedEvent) (bool, error) {
 		return true, errors.New("event.event is nil")
 	}
 
-	if len(f.eventNames) == 0 {
+	if len(f.eventNames) == 0 && len(f.modules) == 0 {
 		return false, nil
 	}
 
-	return f.applyEventNamesFilter(event)
+	if len(f.eventNames) > 0 {
+		shouldDrop, err := f.shouldDropFromEventNames(event)
+		if err != nil {
+			return true, errors.Wrap(err, "failed to apply event names filter")
+		}
+
+		if shouldDrop {
+			return true, nil
+		}
+	}
+
+	if len(f.modules) > 0 {
+		shouldDrop, err := f.shouldDropFromModules(event)
+		if err != nil {
+			return true, errors.Wrap(err, "failed to apply modules filter")
+		}
+
+		if shouldDrop {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
 
-func (f *eventFilter) applyEventNamesFilter(event *DecoratedEvent) (bool, error) {
+func (f *eventFilter) shouldDropFromEventNames(event *DecoratedEvent) (bool, error) {
 	if len(f.eventNames) == 0 {
 		return false, nil
 	}
@@ -80,6 +117,20 @@ func (f *eventFilter) applyEventNamesFilter(event *DecoratedEvent) (bool, error)
 	}
 
 	_, ok := f.eventNames[event.Event.Name.String()]
+
+	return !ok, nil
+}
+
+func (f *eventFilter) shouldDropFromModules(event *DecoratedEvent) (bool, error) {
+	if len(f.modules) == 0 {
+		return false, nil
+	}
+
+	if event.GetMeta().GetClient().GetModule() == 0 {
+		return true, errors.New("event.meta.client.module is empty")
+	}
+
+	_, ok := f.modules[event.GetMeta().GetClient().GetModule().String()]
 
 	return !ok, nil
 }
--- pkg/proto/xatu/mode.go
+++ pkg/proto/xatu/mode.go
@@ -3,12 +3,13 @@ package xatu
 type Mode string
 
 const (
-	ModeUnknown   Mode = ""
-	ModeSentry    Mode = "sentry"
-	ModeCannon    Mode = "cannon"
-	ModeServer    Mode = "server"
-	ModeMimicry   Mode = "mimicry"
-	ModeDiscovery Mode = "discovery"
-	ModeCLMimicry Mode = "cl-mimicry"
-	ModeELMimicry Mode = "el-mimicry"
+	ModeUnknown      Mode = ""
+	ModeSentry       Mode = "sentry"
+	ModeCannon       Mode = "cannon"
+	ModeServer       Mode = "server"
+	ModeMimicry      Mode = "mimicry"
+	ModeDiscovery    Mode = "discovery"
+	ModeCLMimicry    Mode = "cl-mimicry"
+	ModeELMimicry    Mode = "el-mimicry"
+	ModeRelayMonitor Mode = "relay-monitor"
 )

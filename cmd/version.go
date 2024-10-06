3baea70508ff3996549f21826e426d2ee4b28d9a
Andrew Davis <1709934+Savid@users.noreply.github.com>
2024-10-07 09:39:07 +1000

    feat(cmd/version): add version command

---
+++ cmd/version.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	release  = "dev"
+	commit   = "unknown"
+	platform = "unknown"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the version of Xatu.",
+	Long:  `Prints the version of Xatu.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("xatu version %s %s %s\n", version, commit, platform)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}

d4e0ed6c5280d464ccb0a2cbef1fb6fcab0822e5
Sam Calder-Mason <sam.calder-mason@ethereum.org>
2024-10-17 13:57:41 +1000

    feat(docker-compose): Add clickhouse users (#402)
    * feat(docker-compose): Add clickhouse users
    
    * fix: Update password tags to include replace attribute
    
    * fix: Update password replacement value in users.xml
    
    * fix: Update default ClickHouse password in script
    
    * fix: Update ClickHouse password handling in scripts
    
    * fix: Update CLICKHOUSE_PASSWORD handling in config files
    
    * chore: Update ClickHouse password handling in config files
    
    * fix: Update password variable in database connection URL

--- deploy/local/docker-compose/clickhouse/clickhouse-01/etc/clickhouse-server/docker-entrypoint-initdb.d/init-db.sh
+++ deploy/local/docker-compose/clickhouse/clickhouse-01/etc/clickhouse-server/docker-entrypoint-initdb.d/init-db.sh
@@ -1,2 +1,43 @@
 #!/bin/bash
 set -e
+
+cat /etc/clickhouse-server/users.d/users.xml
+
+cat <<EOT >> /etc/clickhouse-server/users.d/default.xml
+<yandex>
+  <users>
+    <${CLICKHOUSE_USER}>
+      <profile>default</profile>
+      <networks>
+        <ip>::/0</ip>
+      </networks>
+      $([ -n "${CLICKHOUSE_PASSWORD}" ] && echo "<password>${CLICKHOUSE_PASSWORD}</password>")
+      <quota>default</quota>
+    </${CLICKHOUSE_USER}>
+    <readonly>
+      <password>${CLICKHOUSE_USER_READONLY_PASSWORD}</password>
+    </readonly>
+  </users>
+</yandex>
+EOT
+
+cat <<EOT >> /etc/clickhouse-server/config.d/users.xml
+<clickhouse replace="true">
+    <remote_servers>
+        <cluster_2S_1R>
+            <shard>
+                <replica>
+                    <host>xatu-clickhouse-01</host>
+                    $([ -n "${CLICKHOUSE_PASSWORD}" ] && echo "<password replace=\"true\">${CLICKHOUSE_PASSWORD}</password>")
+                </replica>
+            </shard>
+            <shard>
+                <replica>
+                    <host>xatu-clickhouse-02</host>
+                    $([ -n "${CLICKHOUSE_PASSWORD}" ] && echo "<password replace=\"true\">${CLICKHOUSE_PASSWORD}</password>")
+                </replica>
+            </shard>
+        </cluster_2S_1R>
+    </remote_servers>
+</clickhouse>
+EOT

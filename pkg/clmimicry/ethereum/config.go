ba734cf95a0c00cd1f08cf5dd2d018b7adf23b8b
Matty Evans <mattevansnz@gmail.com>
2024-11-26 16:37:19 +1000

    feat: Add devnet configuration options for Ethereum

--- pkg/clmimicry/ethereum/config.go
+++ pkg/clmimicry/ethereum/config.go
@@ -1,14 +1,52 @@
 package ethereum
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/prysmaticlabs/prysm/v5/config/params"
+)
 
 type Config struct {
 	Network string `yaml:"network"`
+
+	// Devnet specific attributes (requires 'network=devnet').
+	Devnet DevnetOptions `yaml:"devnet"`
+}
+
+type DevnetOptions struct {
+	// ConfigURL is the .yaml URL to fetch the beacon chain cfg.
+	ConfigURL string `yaml:"configUrl"`
+	// BootnodesURL is the .yaml URL from which to fetch the bootnode ENRs.
+	BootnodesURL string `yaml:"bootnodesUrl"`
+	// DepositContractBlockURL is the .txt URL from which to fetch the deposit contract block.
+	DepositContractBlockURL string `yaml:"depositContractBlockUrl"`
+	// GenesisSSZURL is the .ssz URL from which to fetch the genesis data.
+	GenesisSSZURL string `yaml:"genesisSszUrl"`
 }
 
 func (c *Config) Validate() error {
 	if c.Network == "" {
-		return errors.New("network is required")
+		return errors.New("ethereum.network is required")
+	}
+
+	if c.Network != params.DevnetName {
+		return nil
+	}
+
+	if c.Devnet.ConfigURL == "" {
+		return errors.New("ethereum.network.devnet.configUrl is required")
+	}
+
+	if c.Devnet.BootnodesURL == "" {
+		return errors.New("ethereum.network.devnet.bootnodesUrl is required")
+	}
+
+	if c.Devnet.DepositContractBlockURL == "" {
+		return errors.New("ethereum.network.devnet.depositContractBlockUrl is required")
+	}
+
+	if c.Devnet.GenesisSSZURL == "" {
+		return errors.New("ethereum.network.devnet.genesisSszUrl is required")
 	}
 
 	return nil

ed65ad76f94a61e3442f02bc40f7d6b562d57720
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-05-16 16:26:14 +1000

    add node record events

---
+++ pkg/server/service/event-ingester/event/noderecord/consensus.go
@@ -0,0 +1,46 @@
+package mevrelay
+
+import (
+	"context"
+	"errors"
+
+	"github.com/ethpandaops/xatu/pkg/proto/xatu"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	ConsensusType = xatu.Event_NODE_RECORD_CONSENSUS.String()
+)
+
+type Consensus struct {
+	log   logrus.FieldLogger
+	event *xatu.DecoratedEvent
+}
+
+func NewConsensus(log logrus.FieldLogger, event *xatu.DecoratedEvent) *Consensus {
+	return &Consensus{
+		log:   log.WithField("event", ConsensusType),
+		event: event,
+	}
+}
+
+func (b *Consensus) Type() string {
+	return ConsensusType
+}
+
+func (b *Consensus) Validate(ctx context.Context) error {
+	_, ok := b.event.Data.(*xatu.DecoratedEvent_NodeRecordConsensus)
+	if !ok {
+		return errors.New("failed to cast event data")
+	}
+
+	return nil
+}
+
+func (b *Consensus) Filter(ctx context.Context) bool {
+	return false
+}
+
+func (b *Consensus) AppendServerMeta(ctx context.Context, meta *xatu.ServerMeta) *xatu.ServerMeta {
+	return meta
+}
---
+++ pkg/server/service/event-ingester/event/noderecord/execution.go
@@ -0,0 +1,46 @@
+package mevrelay
+
+import (
+	"context"
+	"errors"
+
+	"github.com/ethpandaops/xatu/pkg/proto/xatu"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	ExecutionType = xatu.Event_NODE_RECORD_EXECUTION.String()
+)
+
+type Execution struct {
+	log   logrus.FieldLogger
+	event *xatu.DecoratedEvent
+}
+
+func NewExecution(log logrus.FieldLogger, event *xatu.DecoratedEvent) *Execution {
+	return &Execution{
+		log:   log.WithField("event", ExecutionType),
+		event: event,
+	}
+}
+
+func (b *Execution) Type() string {
+	return ExecutionType
+}
+
+func (b *Execution) Validate(ctx context.Context) error {
+	_, ok := b.event.Data.(*xatu.DecoratedEvent_NodeRecordExecution)
+	if !ok {
+		return errors.New("failed to cast event data")
+	}
+
+	return nil
+}
+
+func (b *Execution) Filter(ctx context.Context) bool {
+	return false
+}
+
+func (b *Execution) AppendServerMeta(ctx context.Context, meta *xatu.ServerMeta) *xatu.ServerMeta {
+	return meta
+}

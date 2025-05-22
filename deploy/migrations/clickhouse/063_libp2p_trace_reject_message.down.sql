b1c655bc84755f303ac7446bedaced539ad333d9
Matty Evans <mattevansnz@gmail.com>
2025-05-22 10:58:51 +1000

    feat(clickhouse): add migrations for libp2p trace tables
    Adds up and down migration files for new ClickHouse tables to store libp2p trace data.

---
+++ deploy/migrations/clickhouse/063_libp2p_trace_reject_message.down.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS default.libp2p_reject_message ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS default.libp2p_reject_message_local ON CLUSTER '{cluster}';
---
+++ deploy/migrations/clickhouse/065_libp2p_trace_duplicate_message.down.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS default.libp2p_duplicate_message ON CLUSTER '{cluster}';
+DROP TABLE IF EXISTS default.libp2p_duplicate_message_local ON CLUSTER '{cluster}';

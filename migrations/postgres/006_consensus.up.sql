65ea4d033fc3d3912cbc562c127d366fb3dcab23
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-07-24 11:12:48 +1000

    fix(migrations): remove dupe lines

--- migrations/postgres/006_consensus.up.sql
+++ migrations/postgres/006_consensus.up.sql
@@ -1,4 +1,3 @@
-
 ALTER TABLE node_record ALTER COLUMN enr TYPE VARCHAR(1000);
 ALTER TABLE node_record_execution ALTER COLUMN enr TYPE VARCHAR(1000);
 ALTER TABLE node_record_activity ALTER COLUMN enr TYPE VARCHAR(1000);
@@ -26,7 +25,3 @@ CREATE TABLE node_record_consensus (
 CREATE INDEX node_record_consensus_network_id_create_time_idx ON node_record_consensus (network_id, create_time DESC);
 CREATE INDEX node_record_consensus_network_id_fork_digest_create_time_idx ON node_record_consensus (network_id, fork_digest, create_time DESC);
 CREATE INDEX node_record_consensus_create_time_network_id_fork_digest_idx ON node_record_consensus (create_time, network_id, fork_digest);
-
-ALTER TABLE node_record ALTER COLUMN enr TYPE VARCHAR(1000);
-ALTER TABLE node_record_execution ALTER COLUMN enr TYPE VARCHAR(1000);
-ALTER TABLE node_record_activity ALTER COLUMN enr TYPE VARCHAR(1000);

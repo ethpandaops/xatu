700ab600da20a196c117c6e735e8404536390c86
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-08-07 13:10:41 +1000

    add kzg count field

--- deploy/migrations/clickhouse/074_libp2p_gossipsub_data_column_sidecar.up.sql
+++ deploy/migrations/clickhouse/074_libp2p_gossipsub_data_column_sidecar.up.sql
@@ -14,6 +14,7 @@ CREATE TABLE libp2p_gossipsub_data_column_sidecar_local ON CLUSTER '{cluster}'
     `propagation_slot_start_diff` UInt32 COMMENT 'Difference in slot start time for propagation' CODEC(ZSTD(1)),
     `proposer_index` UInt32 COMMENT 'The proposer index of the beacon block' CODEC(ZSTD(1)),
     `column_index` UInt64 COMMENT 'Column index associated with the record' CODEC(ZSTD(1)),
+    `kzg_commitments_count` UInt32 COMMENT 'Number of KZG commitments associated with the record' CODEC(ZSTD(1)),
     `parent_root` FixedString(66) COMMENT 'Parent root of the beacon block' CODEC(ZSTD(1)),
     `state_root` FixedString(66) COMMENT 'State root of the beacon block' CODEC(ZSTD(1)),
     `peer_id_unique_key` Int64 COMMENT 'Unique key associated with the identifier of the peer',

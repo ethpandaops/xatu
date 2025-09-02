b85db63b4bb197888ee1463dc956e4dbc5f3db54
Matty Evans <mattevansnz@gmail.com>
2025-09-02 10:58:40 +1000

    refactor(clickhouse): replace dynamic config generation with idempotent init check
    Remove inline XML and SQL generation scripts to avoid permission
    errors on container restarts. Use a marker file to ensure one-time
    initialization only.

--- deploy/local/docker-compose/clickhouse/clickhouse-01/etc/clickhouse-server/docker-entrypoint-initdb.d/init-db.sh
+++ deploy/local/docker-compose/clickhouse/clickhouse-01/etc/clickhouse-server/docker-entrypoint-initdb.d/init-db.sh
@@ -1,43 +1,14 @@
 #!/bin/bash
 set -e
 
-cat /etc/clickhouse-server/users.d/users.xml
-
-cat <<EOT >> /etc/clickhouse-server/users.d/default.xml
-<yandex>
-  <users>
-    <${CLICKHOUSE_USER}>
-      <profile>default</profile>
-      <networks>
-        <ip>::/0</ip>
-      </networks>
-      $([ -n "${CLICKHOUSE_PASSWORD}" ] && echo "<password>${CLICKHOUSE_PASSWORD}</password>")
-      <quota>default</quota>
-    </${CLICKHOUSE_USER}>
-    <readonly>
-      <password>${CLICKHOUSE_USER_READONLY_PASSWORD}</password>
-    </readonly>
-  </users>
-</yandex>
-EOT
-
-cat <<EOT >> /etc/clickhouse-server/config.d/users.xml
-<clickhouse replace="true">
-    <remote_servers>
-        <cluster_2S_1R>
-            <shard>
-                <replica>
-                    <host>xatu-clickhouse-01</host>
-                    $([ -n "${CLICKHOUSE_PASSWORD}" ] && echo "<password replace=\"true\">${CLICKHOUSE_PASSWORD}</password>")
-                </replica>
-            </shard>
-            <shard>
-                <replica>
-                    <host>xatu-clickhouse-02</host>
-                    $([ -n "${CLICKHOUSE_PASSWORD}" ] && echo "<password replace=\"true\">${CLICKHOUSE_PASSWORD}</password>")
-                </replica>
-            </shard>
-        </cluster_2S_1R>
-    </remote_servers>
-</clickhouse>
-EOT
+# Only create config if files don't already exist (avoid permission issues on restart)
+if [ ! -f /var/lib/clickhouse/init_completed ]; then
+    echo "Running first-time initialization..."
+    
+    # Create a marker file to indicate initialization has been completed
+    touch /var/lib/clickhouse/init_completed || true
+    
+    echo "Initialization completed."
+else
+    echo "Initialization already completed, skipping..."
+fi

80680fda3893b73f83515b0f5ca391e804972e5a
Sam Calder-Mason <sam.calder-mason@ethereum.org>
2025-10-01 12:17:53 +1000

    Apply suggestion from @Savid
    Co-authored-by: Andrew Davis <1709934+Savid@users.noreply.github.com>
    Signed-off-by: Sam Calder-Mason <sam@puritydev.io>

--- deploy/migrations/clickhouse/078_add_direction_to_metadata_status.up.sql
+++ deploy/migrations/clickhouse/078_add_direction_to_metadata_status.up.sql
@@ -4,19 +4,13 @@ DROP TABLE IF EXISTS libp2p_handle_metadata ON CLUSTER '{cluster}' SYNC;
 
 -- Add Direction column to libp2p_handle_status_local
 ALTER TABLE libp2p_handle_status_local ON CLUSTER '{cluster}'
-ADD COLUMN IF NOT EXISTS direction LowCardinality(Nullable(String)) CODEC(ZSTD(1))
+ADD COLUMN IF NOT EXISTS direction LowCardinality(Nullable(String)) COMMENT 'Direction of the RPC request (inbound or outbound)' CODEC(ZSTD(1))
 AFTER protocol;
-
-ALTER TABLE libp2p_handle_status_local ON CLUSTER '{cluster}'
-COMMENT COLUMN direction 'Direction of the RPC request (inbound or outbound)';
 
 -- Add Direction column to libp2p_handle_metadata_local
 ALTER TABLE libp2p_handle_metadata_local ON CLUSTER '{cluster}'
-ADD COLUMN IF NOT EXISTS direction LowCardinality(Nullable(String)) CODEC(ZSTD(1))
-AFTER protocol;
-
-ALTER TABLE libp2p_handle_metadata_local ON CLUSTER '{cluster}'
-COMMENT COLUMN direction 'Direction of the RPC request (inbound or outbound)';
+ADD COLUMN IF NOT EXISTS direction LowCardinality(Nullable(String)) COMMENT 'Direction of the RPC request (inbound or outbound)' CODEC(ZSTD(1))
+AFTER protocol;```
 
 -- Recreate distributed tables
 CREATE TABLE libp2p_handle_status ON CLUSTER '{cluster}' AS libp2p_handle_status_local

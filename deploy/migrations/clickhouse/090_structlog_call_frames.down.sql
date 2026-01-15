97969172fd2c902769f6d09d136a29b317cbfb93
Matty Evans <mattevansnz@gmail.com>
2026-01-16 08:58:55 +1000

    fix(migrations): remove transaction_gas_used column from 090 migration The column was added and dropped in the same migration, making the migration non-idempotent and causing issues during rollback.

--- deploy/migrations/clickhouse/090_structlog_call_frames.down.sql
+++ deploy/migrations/clickhouse/090_structlog_call_frames.down.sql
@@ -15,9 +15,3 @@ ALTER TABLE canonical_execution_transaction_structlog_local ON CLUSTER '{cluster
 
 ALTER TABLE canonical_execution_transaction_structlog ON CLUSTER '{cluster}'
     DROP COLUMN gas_self;
-
-ALTER TABLE canonical_execution_transaction_structlog_local ON CLUSTER '{cluster}'
-    DROP COLUMN transaction_gas_used;
-
-ALTER TABLE canonical_execution_transaction_structlog ON CLUSTER '{cluster}'
-    DROP COLUMN transaction_gas_used;

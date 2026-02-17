317232b0b115ffc3af8354a0e71f89a31312e03b
Matty Evans <mattevansnz@gmail.com>
2026-02-17 11:25:11 +1000

    feat(clickhouse): add memory_expansion_gas column to structlog agg tables chore(clickhouse): reorder columns so memory_expansion_gas precedes cold_access_count

--- deploy/migrations/clickhouse/102_structlog_agg_resource_gas_columns.down.sql
+++ deploy/migrations/clickhouse/102_structlog_agg_resource_gas_columns.down.sql
@@ -3,6 +3,7 @@ ALTER TABLE canonical_execution_transaction_structlog_agg_local ON CLUSTER '{clu
     DROP COLUMN IF EXISTS `memory_words_sum_after`,
     DROP COLUMN IF EXISTS `memory_words_sq_sum_before`,
     DROP COLUMN IF EXISTS `memory_words_sq_sum_after`,
+    DROP COLUMN IF EXISTS `memory_expansion_gas`,
     DROP COLUMN IF EXISTS `cold_access_count`;
 
 ALTER TABLE canonical_execution_transaction_structlog_agg ON CLUSTER '{cluster}'
@@ -10,4 +11,5 @@ ALTER TABLE canonical_execution_transaction_structlog_agg ON CLUSTER '{cluster}'
     DROP COLUMN IF EXISTS `memory_words_sum_after`,
     DROP COLUMN IF EXISTS `memory_words_sq_sum_before`,
     DROP COLUMN IF EXISTS `memory_words_sq_sum_after`,
+    DROP COLUMN IF EXISTS `memory_expansion_gas`,
     DROP COLUMN IF EXISTS `cold_access_count`;

4e3bda7ccb8fd0217bc81adf3c19c131db63d98c
Sam Calder-Mason <sam@puritydev.io>
2026-06-23 15:00:15 +1000

    cannon: implement execution_request_withdrawal deriver + server handler + clickhouse route

--- pkg/clickhouse/route/canonical/canonical_beacon_block_execution_request_withdrawal.go
+++ pkg/clickhouse/route/canonical/canonical_beacon_block_execution_request_withdrawal.go
@@ -2,13 +2,15 @@ package canonical
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/ethpandaops/xatu/pkg/clickhouse/route"
 	"github.com/ethpandaops/xatu/pkg/proto/xatu"
 )
 
-// TODO: Add the xatu.Event_* name(s) that route events to the canonical_beacon_block_execution_request_withdrawal table.
-var canonicalBeaconBlockExecutionRequestWithdrawalEventNames = []xatu.Event_Name{}
+var canonicalBeaconBlockExecutionRequestWithdrawalEventNames = []xatu.Event_Name{
+	xatu.Event_BEACON_API_ETH_V2_BEACON_BLOCK_EXECUTION_REQUEST_WITHDRAWAL,
+}
 
 func init() {
 	r, err := route.NewStaticRoute(
@@ -27,17 +29,79 @@ func init() {
 	}
 }
 
-func (b *canonicalBeaconBlockExecutionRequestWithdrawalBatch) FlattenTo(
-	event *xatu.DecoratedEvent,
-) error {
-	// TODO: Implement this method to flatten the event into columnar batch columns.
-	// The generated .gen.go file contains the available column fields for this table.
-	//
-	// Typical structure:
-	//   b.appendRuntime(event)
-	//   b.appendMetadata(event)
-	//   b.appendPayload(event)
-	//   b.rows++
-	//   return nil
-	return fmt.Errorf("canonicalBeaconBlockExecutionRequestWithdrawal: FlattenTo not implemented")
+func (b *canonicalBeaconBlockExecutionRequestWithdrawalBatch) FlattenTo(event *xatu.DecoratedEvent) error {
+	if event == nil || event.GetEvent() == nil {
+		return nil
+	}
+
+	if event.GetEthV2BeaconBlockExecutionRequestWithdrawal() == nil {
+		return fmt.Errorf("nil eth_v2_beacon_block_execution_request_withdrawal payload: %w", route.ErrInvalidEvent)
+	}
+
+	if err := b.validate(event); err != nil {
+		return err
+	}
+
+	b.appendRuntime(event)
+	b.appendMetadata(event)
+	b.appendPayload(event)
+	b.appendAdditionalData(event)
+	b.rows++
+
+	return nil
+}
+
+func (b *canonicalBeaconBlockExecutionRequestWithdrawalBatch) validate(event *xatu.DecoratedEvent) error {
+	payload := event.GetEthV2BeaconBlockExecutionRequestWithdrawal()
+
+	if payload.GetSourceAddress() == nil {
+		return fmt.Errorf("nil SourceAddress: %w", route.ErrInvalidEvent)
+	}
+
+	if payload.GetValidatorPubkey() == nil {
+		return fmt.Errorf("nil ValidatorPubkey: %w", route.ErrInvalidEvent)
+	}
+
+	if payload.GetAmount() == nil {
+		return fmt.Errorf("nil Amount: %w", route.ErrInvalidEvent)
+	}
+
+	return nil
+}
+
+func (b *canonicalBeaconBlockExecutionRequestWithdrawalBatch) appendRuntime(_ *xatu.DecoratedEvent) {
+	b.UpdatedDateTime.Append(time.Now())
+}
+
+func (b *canonicalBeaconBlockExecutionRequestWithdrawalBatch) appendPayload(event *xatu.DecoratedEvent) {
+	withdrawal := event.GetEthV2BeaconBlockExecutionRequestWithdrawal()
+
+	b.SourceAddress.Append([]byte(withdrawal.GetSourceAddress().GetValue()))
+	b.ValidatorPubkey.Append(withdrawal.GetValidatorPubkey().GetValue())
+	b.Amount.Append(withdrawal.GetAmount().GetValue())
+}
+
+//nolint:gosec // G115: proto uint64 values are bounded by ClickHouse uint32 column schema
+func (b *canonicalBeaconBlockExecutionRequestWithdrawalBatch) appendAdditionalData(event *xatu.DecoratedEvent) {
+	additional := event.GetMeta().GetClient().GetEthV2BeaconBlockExecutionRequestWithdrawal()
+	if additional == nil {
+		b.Slot.Append(0)
+		b.SlotStartDateTime.Append(time.Time{})
+		b.Epoch.Append(0)
+		b.EpochStartDateTime.Append(time.Time{})
+		b.BlockVersion.Append("")
+		b.BlockRoot.Append(nil)
+		b.PositionInBlock.Append(0)
+
+		return
+	}
+
+	appendBlockIdentifier(additional.GetBlock(),
+		&b.Slot, &b.SlotStartDateTime, &b.Epoch, &b.EpochStartDateTime, &b.BlockVersion, &b.BlockRoot)
+
+	if position := additional.GetPositionInBlock(); position != nil {
+		b.PositionInBlock.Append(uint32(position.GetValue()))
+	} else {
+		b.PositionInBlock.Append(0)
+	}
 }
--- pkg/clickhouse/route/canonical/canonical_beacon_block_execution_request_withdrawal_test.go
+++ pkg/clickhouse/route/canonical/canonical_beacon_block_execution_request_withdrawal_test.go
@@ -3,25 +3,41 @@ package canonical
 import (
 	"testing"
 
+	"google.golang.org/protobuf/types/known/wrapperspb"
+
 	"github.com/ethpandaops/xatu/pkg/clickhouse/route/testfixture"
+	ethv1 "github.com/ethpandaops/xatu/pkg/proto/eth/v1"
 	"github.com/ethpandaops/xatu/pkg/proto/xatu"
 )
 
 func TestSnapshot_canonical_beacon_block_execution_request_withdrawal(t *testing.T) {
-	if len(canonicalBeaconBlockExecutionRequestWithdrawalEventNames) == 0 {
-		t.Skip("no event names registered for canonical_beacon_block_execution_request_withdrawal")
-	}
-
 	testfixture.AssertSnapshot(t, newcanonicalBeaconBlockExecutionRequestWithdrawalBatch(), &xatu.DecoratedEvent{
 		Event: &xatu.Event{
-			Name:     canonicalBeaconBlockExecutionRequestWithdrawalEventNames[0],
+			Name:     xatu.Event_BEACON_API_ETH_V2_BEACON_BLOCK_EXECUTION_REQUEST_WITHDRAWAL,
 			DateTime: testfixture.TS(),
-			Id:       "snapshot-1",
+			Id:       "cberw-1",
 		},
-		Meta: testfixture.BaseMeta(),
-		// TODO: Add event-specific Data field and MetaWithAdditional for richer assertions.
+		Meta: testfixture.MetaWithAdditional(&xatu.ClientMeta{
+			AdditionalData: &xatu.ClientMeta_EthV2BeaconBlockExecutionRequestWithdrawal{
+				EthV2BeaconBlockExecutionRequestWithdrawal: &xatu.ClientMeta_AdditionalEthV2BeaconBlockExecutionRequestWithdrawalData{
+					Block: &xatu.BlockIdentifier{
+						Epoch: testfixture.EpochAdditional(),
+					},
+					PositionInBlock: wrapperspb.UInt64(2),
+				},
+			},
+		}),
+		Data: &xatu.DecoratedEvent_EthV2BeaconBlockExecutionRequestWithdrawal{
+			EthV2BeaconBlockExecutionRequestWithdrawal: &ethv1.ElectraExecutionRequestWithdrawal{
+				SourceAddress:   wrapperspb.String("0x000000000000000000000000000000000000dead"),
+				ValidatorPubkey: wrapperspb.String("0xabcdef"),
+				Amount:          wrapperspb.UInt64(32000000000),
+			},
+		},
 	}, 1, map[string]any{
-		"meta_client_name": "test-client",
-		// TODO: Add payload-specific column assertions.
+		"source_address":    "0x000000000000000000000000000000000000dead",
+		"validator_pubkey":  "0xabcdef",
+		"amount":            uint64(32000000000),
+		"position_in_block": uint32(2),
 	})
 }

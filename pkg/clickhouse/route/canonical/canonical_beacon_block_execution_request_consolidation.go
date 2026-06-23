20d3288b021eb2d6251e75b2302aebc48c96f126
Sam Calder-Mason <sam@puritydev.io>
2026-06-23 14:57:37 +1000

    cannon: implement execution_request_consolidation deriver + server handler + clickhouse route

--- pkg/clickhouse/route/canonical/canonical_beacon_block_execution_request_consolidation.go
+++ pkg/clickhouse/route/canonical/canonical_beacon_block_execution_request_consolidation.go
@@ -2,13 +2,15 @@ package canonical
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/ethpandaops/xatu/pkg/clickhouse/route"
 	"github.com/ethpandaops/xatu/pkg/proto/xatu"
 )
 
-// TODO: Add the xatu.Event_* name(s) that route events to the canonical_beacon_block_execution_request_consolidation table.
-var canonicalBeaconBlockExecutionRequestConsolidationEventNames = []xatu.Event_Name{}
+var canonicalBeaconBlockExecutionRequestConsolidationEventNames = []xatu.Event_Name{
+	xatu.Event_BEACON_API_ETH_V2_BEACON_BLOCK_EXECUTION_REQUEST_CONSOLIDATION,
+}
 
 func init() {
 	r, err := route.NewStaticRoute(
@@ -27,17 +29,52 @@ func init() {
 	}
 }
 
-func (b *canonicalBeaconBlockExecutionRequestConsolidationBatch) FlattenTo(
-	event *xatu.DecoratedEvent,
-) error {
-	// TODO: Implement this method to flatten the event into columnar batch columns.
-	// The generated .gen.go file contains the available column fields for this table.
-	//
-	// Typical structure:
-	//   b.appendRuntime(event)
-	//   b.appendMetadata(event)
-	//   b.appendPayload(event)
-	//   b.rows++
-	//   return nil
-	return fmt.Errorf("canonicalBeaconBlockExecutionRequestConsolidation: FlattenTo not implemented")
+func (b *canonicalBeaconBlockExecutionRequestConsolidationBatch) FlattenTo(event *xatu.DecoratedEvent) error {
+	if event == nil || event.GetEvent() == nil {
+		return nil
+	}
+
+	if event.GetEthV2BeaconBlockExecutionRequestConsolidation() == nil {
+		return fmt.Errorf("nil eth_v2_beacon_block_execution_request_consolidation payload: %w", route.ErrInvalidEvent)
+	}
+
+	b.appendRuntime(event)
+	b.appendMetadata(event)
+	b.appendPayload(event)
+	b.appendAdditionalData(event)
+	b.rows++
+
+	return nil
+}
+
+func (b *canonicalBeaconBlockExecutionRequestConsolidationBatch) appendRuntime(_ *xatu.DecoratedEvent) {
+	b.UpdatedDateTime.Append(time.Now())
+}
+
+func (b *canonicalBeaconBlockExecutionRequestConsolidationBatch) appendPayload(event *xatu.DecoratedEvent) {
+	consolidation := event.GetEthV2BeaconBlockExecutionRequestConsolidation()
+	b.SourceAddress.Append([]byte(consolidation.GetSourceAddress().GetValue()))
+	b.SourcePubkey.Append(consolidation.GetSourcePubkey().GetValue())
+	b.TargetPubkey.Append(consolidation.GetTargetPubkey().GetValue())
+}
+
+//nolint:gosec // G115: proto uint64 values are bounded by ClickHouse uint32 column schema
+func (b *canonicalBeaconBlockExecutionRequestConsolidationBatch) appendAdditionalData(event *xatu.DecoratedEvent) {
+	additional := event.GetMeta().GetClient().GetEthV2BeaconBlockExecutionRequestConsolidation()
+	if additional == nil {
+		b.Slot.Append(0)
+		b.SlotStartDateTime.Append(time.Time{})
+		b.Epoch.Append(0)
+		b.EpochStartDateTime.Append(time.Time{})
+		b.BlockVersion.Append("")
+		b.BlockRoot.Append(nil)
+		b.PositionInBlock.Append(0)
+
+		return
+	}
+
+	appendBlockIdentifier(additional.GetBlock(),
+		&b.Slot, &b.SlotStartDateTime, &b.Epoch, &b.EpochStartDateTime, &b.BlockVersion, &b.BlockRoot)
+
+	b.PositionInBlock.Append(uint32(additional.GetPositionInBlock().GetValue()))
 }

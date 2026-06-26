117539efe50bb689238244f02837e075dc7ec320
Sam Calder-Mason <sam@puritydev.io>
2026-06-26 14:53:08 +1000

    canonical_beacon_block_voluntary_exit: strict validate (error-if-absent on required fields)

--- pkg/clickhouse/route/canonical/canonical_beacon_block_voluntary_exit_test.go
+++ pkg/clickhouse/route/canonical/canonical_beacon_block_voluntary_exit_test.go
@@ -3,6 +3,8 @@ package canonical
 import (
 	"testing"
 
+	"google.golang.org/protobuf/types/known/wrapperspb"
+
 	"github.com/ethpandaops/xatu/pkg/clickhouse/route/testfixture"
 	ethv1 "github.com/ethpandaops/xatu/pkg/proto/eth/v1"
 	"github.com/ethpandaops/xatu/pkg/proto/xatu"
@@ -19,15 +21,27 @@ func TestSnapshot_canonical_beacon_block_voluntary_exit(t *testing.T) {
 			AdditionalData: &xatu.ClientMeta_EthV2BeaconBlockVoluntaryExit{
 				EthV2BeaconBlockVoluntaryExit: &xatu.ClientMeta_AdditionalEthV2BeaconBlockVoluntaryExitData{
 					Block: &xatu.BlockIdentifier{
-						Epoch: testfixture.EpochAdditional(),
+						Slot:    testfixture.SlotEpochAdditional(),
+						Epoch:   testfixture.EpochAdditional(),
+						Version: "deneb",
+						Root:    "0x1111111111111111111111111111111111111111111111111111111111111111",
 					},
 				},
 			},
 		}),
 		Data: &xatu.DecoratedEvent_EthV2BeaconBlockVoluntaryExit{
-			EthV2BeaconBlockVoluntaryExit: &ethv1.SignedVoluntaryExitV2{},
+			EthV2BeaconBlockVoluntaryExit: &ethv1.SignedVoluntaryExitV2{
+				Message: &ethv1.VoluntaryExitV2{
+					Epoch:          wrapperspb.UInt64(3),
+					ValidatorIndex: wrapperspb.UInt64(42),
+				},
+				Signature: "0x" + "ab" + "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+			},
 		},
 	}, 1, map[string]any{
-		"meta_network_name": "mainnet",
+		"meta_network_name":                      "mainnet",
+		"block_version":                          "deneb",
+		"voluntary_exit_message_epoch":           uint32(3),
+		"voluntary_exit_message_validator_index": uint32(42),
 	})
 }
